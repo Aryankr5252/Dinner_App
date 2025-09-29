353db9fe901938a3833d5277ba7a357e8c231058
Aryankr5252 <aryanak5252@gmail.com>
2025-09-29 16:44:58 +0530

    initials

---
+++ app/testing/index.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const testing = () => {
+  return (
+    <View>
+      <Text>
+        Testing
+      </Text>
+    </View>
+  )
+}
+
+export default testing
